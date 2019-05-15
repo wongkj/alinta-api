e99c96e645fbaf5bd47162c283aa6261a9e6b081
Jason Wong <wongkj.11@gmail.com>
2019-05-16 07:19:50 +1000

    Started Testing

--- alintaApi/alintaApi/Data/CustomersDbContext.cs
+++ alintaApi/alintaApi/Data/CustomersDbContext.cs
@@ -8,10 +8,59 @@ using alintaApi.Models;
 namespace alintaApi.Data
 {
     public class CustomersDbContext : DbContext
-    {
+    { 
         public CustomersDbContext(DbContextOptions<CustomersDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData
+            (
+                new Customer
+                {
+                    Id = 1,
+                    firstName = "Steve",
+                    lastName = "Rogers",
+                    dateOfBirth = "05/05/1910"
+                },
+                new Customer
+                {
+                    Id = 2,
+                    firstName = "Tony",
+                    lastName = "Stark",
+                    dateOfBirth = "06/06/1960"
+                },
+                new Customer
+                {
+                    Id = 3,
+                    firstName = "Peter",
+                    lastName = "Parker",
+                    dateOfBirth = "02/02/2002"
+                },
+                new Customer
+                {
+                    Id = 4,
+                    firstName = "Steven",
+                    lastName = "Strange",
+                    dateOfBirth = "04/04/1975"
+                },
+                new Customer
+                {
+                    Id = 5,
+                    firstName = "Natasha",
+                    lastName = "Romanov",
+                    dateOfBirth = "01/01/1985"
+                },
+                new Customer
+                {
+                    Id = 6,
+                    firstName = "Bruce",
+                    lastName = "Banner",
+                    dateOfBirth = "07/07/1970"
+                }
+            );
         }
 
         public DbSet<Customer> Customers { get; set; }
--- alintaApi/alintaApiTests/CustomersControllerTests.cs
+++ alintaApi/alintaApiTests/CustomersControllerTests.cs
@@ -1,22 +1,49 @@
 using System;
 using alintaApi.Controllers;
+using alintaApi.Models;
+using alintaApi.Data;
 using Xunit;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using Autofac.Extras.Moq;
 
 namespace alintaApiTests
 {
-
+    public interface IDbContext : IDisposable
+    {
+        
+    }
 
     public class CustomersControllerTests
     {
+        private CustomersController controller;
+
         [Fact]
-        public void Get_CheckResultNotNull_ReturnObject()
+        public void ShouldReturnAllCustomers()
         {
+            var options = new DbContextOptionsBuilder<CustomersDbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomersDB")
+                .Options;
+            var context = new CustomersDbContext(options);
+            Seed(context);
+            var query = new CustomersController(context);
+            var result = query.Get(sort: "asc");
+            // Assert.Equal(5, result.Count)
+        }
 
-            
-            double expected = 5;
-            double actual = 5;
+        private void Seed(CustomersDbContext context)
+        {
+            var customers = new[]
+            {
+                new Customer { Id = 1, firstName = "Bruce", lastName = "Banner", dateOfBirth = "01/01/2001" },
+                new Customer { Id = 2, firstName = "Peter", lastName = "Parker", dateOfBirth = "02/02/1980" },
+                new Customer { Id = 3, firstName = "Steve", lastName = "Rogers", dateOfBirth = "03/03/1975" },
+                new Customer { Id = 4, firstName = "Tony", lastName = "Stark", dateOfBirth = "04/04/1970" },
+                new Customer { Id = 5, firstName = "Steven", lastName = "Strange", dateOfBirth = "05/05/1965" }
+            };
 
-            Assert.Equal(expected, actual);
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
         }
     }
 }
